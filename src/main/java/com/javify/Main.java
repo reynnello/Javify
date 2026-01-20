85ffb8be5525b65d48bbba27b491715376c107ee
Anton Reynov <re1yn9524@gmail.com>
2026-01-20 09:31:48 +0000

    start

---
+++ src/main/java/com/javify/Main.java
@@ -0,0 +1,14 @@
+package com.javify;
+
+import java.sql.*;
+
+public class Main {
+    private static final String DB_PATH = "db/users.db";
+    private static final String URL = "jdbc:sqlite:" + DB_PATH;
+
+    public static void initDatabase() {
+
+    }
+
+
+}
